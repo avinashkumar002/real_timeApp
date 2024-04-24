a52d129f9b4a51e057fdaf62e42ce73779205d88
avinashkumar002 <avinashsingh30oct@gmail.com>
2024-04-24 20:34:33 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,101 @@
+import {io} from "socket.io-client"
+import { useEffect , useState , useMemo} from "react";
+import {Container,Typography,TextField,Button , Box , Stack} from '@mui/material'
+
+const App = () => {
+  const socket = useMemo(() => io("http://localhost:3000",{
+    withCredentials: true,
+  }),
+   []
+  );
+  
+  const [messages , setMessages ] = useState([]);
+  const [message, setMessage] =  useState("");
+  const [room, setRoom] = useState("");
+  const [socketID, setSocketId] = useState("");
+  const [roomName, setRoomName] = useState("");
+
+  console.log(messages);
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    socket.emit("message",{message,room});
+    setMessage("");
+  };
+
+  const joinRoomHandler = (e)=>{
+    e.preventDefault();
+    socket.emit("join-room", roomName);
+    setRoomName("");
+  }
+
+  useEffect(() => {
+   socket.on("connect", ()=>{
+    setSocketId(socket.id);
+    console.log("connected",socket.id);
+   }) ;
+
+   socket.on("recieve-message",(data)=>{
+    console.log(data);
+    setMessages((messages) => [...messages , data]);
+   });
+   socket.on("welcome",(s)=>{
+    console.log(s);
+   });
+   return ()=>{
+    socket.disconnect();
+   }
+  }, []);
+
+
+  return (
+    <Container maxWidth="sm">
+      <Box sx={{height:200}} />
+      <Typography variant="h6" component="div" gutterBottom>
+        {socketID}
+      </Typography>
+
+      <form action="" onSubmit={joinRoomHandler}>
+      <h5>Join Room</h5>
+      <TextField 
+       value={roomName} 
+       onChange={(e)=>setRoomName(e.target.value)}
+       id="outlined-basic" 
+       label="Room Name" 
+       variant="outlined" 
+       />
+       <Button type="submit" variant="contained" color="primary">
+        Join
+        </Button> 
+      </form>
+
+      <form onSubmit={handleSubmit}>
+       <TextField 
+       value={message} 
+       onChange={(e)=>setMessage(e.target.value)}
+       id="outlined-basic" 
+       label="Message" 
+       variant="outlined" 
+       />
+       <TextField 
+       value={room} 
+       onChange={(e)=>setRoom(e.target.value)}
+       id="outlined-basic" 
+       label="Room" 
+       variant="outlined" 
+       />
+       <Button type="submit" variant="contained" color="primary">Send</Button> 
+      </form>
+
+      <Stack>
+        {messages.map((m,i) =>(
+          <Typography key={i} variant="h5" component="div" gutterBottom>
+        {m}
+      </Typography>
+        ))}
+      </Stack>
+    </Container>
+  )
+}
+
+export default App
